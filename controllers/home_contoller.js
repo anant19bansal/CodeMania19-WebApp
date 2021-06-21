d8c7746087329bfabd1f9172455ef4fd84e747d7
Anant Bansal <anant19bansal@gmail.com>
2021-06-21 13:57:23 +0530

    display the comments and their authors along with the posts

--- controllers/home_contoller.js
+++ controllers/home_contoller.js
@@ -4,6 +4,15 @@ module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('id','25');
 
+    // return res.render('home',{
+    //     title:"Home",
+    //     all_posts: posts,
+    // });
+
+
+    // return res.end('<h1>Express is up for Codeial! </h1>');
+
+
     // Post.find({}, function(err, posts){
     //     if(err){console.log('err in finding the posts'); return;}
     //     return res.render('home',{
@@ -12,19 +21,27 @@ module.exports.home = function(req, res){
     //     });
     // });
 
-    Post.find({}).populate('user').exec(function(err, posts){
+    // Post.find({}).populate('user').exec(function(err, posts){
+    //     if(err){console.log('err in finding the posts'); return;}
+    //     return res.render('home',{
+    //         title:"Home",
+    //         posts: posts,
+    //     });
+    // });
+
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate: {
+            path:'user',
+        },
+    })
+    .exec(function(err, posts){
         if(err){console.log('err in finding the posts'); return;}
         return res.render('home',{
             title:"Home",
             posts: posts,
         });
     });
-
-    // return res.render('home',{
-    //     title:"Home",
-    //     all_posts: posts,
-    // });
-
-
-    // return res.end('<h1>Express is up for Codeial! </h1>');
 };
