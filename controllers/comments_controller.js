5a73fca959d639f8d6c8d3011b3183317a39fe05
Anant Bansal <anant19bansal@gmail.com>
2021-06-21 19:56:14 +0530

    add delete posts and delete comments functionality (authorized delete obviously)

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -16,8 +16,47 @@ module.exports.create = function(req, res){
                 return res.redirect('/');
             });
         }
+    });  
+}
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id, function(err, comment){
+        if(comment.user == req.user.id){
+            let postId = comment.post;
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}}, function(err, post){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
     });
     
     
-    
+    // another way to do...it was working fine :) just remember to do .save() after updation
+    // used query params instead of params 
+
+    // let postId = req.query.postId;
+    // let commentId = req.query.commentId;
+
+    // Comment.findById(commentId, function(err, comment){
+    //     // console.log(comment);
+    //     if(comment.user == req.user.id){
+    //         comment.remove();
+    //         Post.findById(postId, function(err, post){
+    //             // console.log(post);
+    //             let deleteIndex = post.comments.findIndex(id => id==commentId);
+    //             // console.log(deleteIndex);
+    //             if(deleteIndex!=-1){
+    //                 post.comments.splice(deleteIndex,1);
+    //                 post.save();
+    //             }
+    //             return res.redirect('back');
+    //         });
+    //     }else{
+    //         console.log('user nahi hai sahi');
+    //         return res.redirect('back');
+    //     }
+    // });
 }
