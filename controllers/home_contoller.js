734d3351a32a4a5602788797293f816d16dde020
Anant Bansal <anant19bansal@gmail.com>
2021-06-21 11:48:12 +0530

    display all posts and creater of that post

--- controllers/home_contoller.js
+++ controllers/home_contoller.js
@@ -1,8 +1,30 @@
+const Post = require('../models/posts');
+
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('id','25');
-    return res.render('home',{
-        title:"Home",
+
+    // Post.find({}, function(err, posts){
+    //     if(err){console.log('err in finding the posts'); return;}
+    //     return res.render('home',{
+    //         title:"Home",
+    //         posts: posts,
+    //     });
+    // });
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        if(err){console.log('err in finding the posts'); return;}
+        return res.render('home',{
+            title:"Home",
+            posts: posts,
+        });
     });
+
+    // return res.render('home',{
+    //     title:"Home",
+    //     all_posts: posts,
+    // });
+
+
     // return res.end('<h1>Express is up for Codeial! </h1>');
 };
